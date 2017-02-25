90c95d4b6d0e2d7093d81092d8639c676e15a19a
Paul Hayes <water7earth@gmail.com>
2017-02-25 07:17:03 +0100

    wip

--- pages/articles.js
+++ pages/articles.js
@@ -9,20 +9,12 @@ export default class extends React.Component {
     const { p } = query
     const page = Number(p || 1)
     const articles = await getArticles('blog', { page })
-    // console.log('query getInitialProps', query)
-    // console.log('page getInitialProps', page)
-    // console.log('articles getInitialProps', articles)
     return { page, articles }
   }
 
   render () {
-
     const { page, url, articles } = this.props
     const offset = (page - 1) * 12
-    // console.log('page', page);
-    // console.log('url', url);
-    // console.log('articles', articles);
-    // console.log('offset', offset);
     return <Page>
       <Articles page={page} offset={offset} articles={articles} />
     </Page>

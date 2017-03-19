0a560657159f0bef78a6782d4a437ff47937b499
Marcin Polski <marcin.polski98@gmail.com>
2017-03-19 13:03:36 +0100

    Read more button and only title is link

--- components/article-teaser-view.js
+++ components/article-teaser-view.js
@@ -2,22 +2,23 @@ import Link from 'next/prefetch'
 
 const ArticleTeaserView = ({ id, title, date, lead, author, teaser }) => (
   <div className="article-teaser-view">
-    <Link href={`/article?id=${id}`}>
-      <a className="article-teaser-view__link">
-        <div className="article-teaser-view__wrapper">
-          <img src={teaser} />
-          <div className="title">
-            <h3>{title}</h3>
-          </div>
-          <div className="meta">
-            {author} | {date}
-          </div>
-          <div className="lead">
-            {lead}
-          </div>
+      <div className="article-teaser-view__wrapper">
+        <img src={teaser} />
+        <Link href={`/article?id=${id}`}>
+          <a className="article-teaser-view__link">
+            <div className="title">
+              <h3>{title}</h3>
+            </div>
+          </a>
+        </Link>
+        <div className="meta">
+          {author} | {date}
         </div>
-      </a>
-    </Link>
+        <div className="lead">
+          {lead}
+        </div>
+      </div>
+    <Link href={`/article?id=${id}`}><a className="article-teaser-view__link">Read More</a></Link>
     <style jsx>{`
 
       a {
@@ -51,7 +52,8 @@ ArticleTeaserView.propTypes = {
   title: React.PropTypes.string.isRequired,
   lead: React.PropTypes.string.isRequired,
   date: React.PropTypes.string.isRequired,
-  author: React.PropTypes.string.isRequired
+  author: React.PropTypes.string.isRequired,
+  teaser: React.PropTypes.string.isRequired,
 }
 
 export default ArticleTeaserView

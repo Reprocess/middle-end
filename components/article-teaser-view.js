21cbc1694d2481c69dd5ebcf3fb747568d96db40
Paul Hayes <water7earth@gmail.com>
2017-05-02 09:03:53 +0200

    updated to latest version of zeit next

--- components/article-teaser-view.js
+++ components/article-teaser-view.js
@@ -1,16 +1,16 @@
-import Link from 'next/prefetch'
+import Link from 'next/link'
 import Moment from 'react-moment'
 import moment from 'moment'
 
 const ArticleTeaserView = ({ id, title, date, lead, author, teaser }) => (
   <div className="article-teaser-view">
         <div className="article-teaser-view__wrapper">
-          <Link href={`/article?id=${id}`}>
+          <Link prefetch href={`/article?id=${id}`}>
             <a className="article-teaser-view__link">
               <img src={teaser} />
             </a>
           </Link>
-          <Link href={`/article?id=${id}`}>
+          <Link prefetch href={`/article?id=${id}`}>
             <a className="article-teaser-view__link">
               <div className="title">
                 <h3>{title}</h3>
@@ -20,14 +20,14 @@ const ArticleTeaserView = ({ id, title, date, lead, author, teaser }) => (
           <div className="meta">
             {author} | <Moment format={(new Date().getFullYear() - new Date(date).getFullYear()) > 0 ? "MMMM DD, YYYY" : "MMMM DD"}>{date}</Moment>
           </div>
-          <Link href={`/article?id=${id}`}>
+          <Link prefetch href={`/article?id=${id}`}>
             <a className="article-teaser-view__link">
               <div className="lead">
                 {lead}
               </div>
             </a>
           </Link>
-          <Link href={`/article?id=${id}`}>
+          <Link prefetch href={`/article?id=${id}`}>
             <a className="article-teaser-view__link">
               <div className="read-more">
                 Read more...
